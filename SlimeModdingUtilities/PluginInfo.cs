0b65003d9e9a428e566fcdb1d201b55fd467c641
Pink <62712899+BSMGPink@users.noreply.github.com>
2021-12-23 16:02:30 +0000

    Slime -> Spin
    Removed NASB Related methods

--- SlimeModdingUtilities/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SMU
-{
-    class PluginInfo
-    {
-        public const string PLUGIN_GUID = "com.steven.nasb.moddingutils";
-        public const string PLUGIN_NAME = "Slime Modding Utilities";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
