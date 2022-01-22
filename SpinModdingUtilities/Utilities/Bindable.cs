ffcb26aad76f1529adab904051f8b45103210e3d
Dominic Aglialoro <domiaglialoro@gmail.com>
2022-01-21 17:06:03 -0800

    Bindable

---
+++ SpinModdingUtilities/Utilities/Bindable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SMU.Utilities {
+    /// <summary>
+    /// Class that stores a value and invokes an event when the value is changed
+    /// </summary>
+    /// <typeparam name="T">The type of the stored value</typeparam>
+    public class Bindable<T> : IReadOnlyBindable<T> {
+        /// <inheritdoc/>
+        public T Value {
+            get => value;
+            set {
+                this.value = value;
+                onChanged?.Invoke(value);
+            }
+        }
+        
+        /// <summary>
+        /// Constructor. Uses the default value for T
+        /// </summary>
+        public Bindable() => value = default;
+
+        /// <summary>
+        /// Constructor. Assigns an initial value
+        /// </summary>
+        /// <param name="value">The initial value</param>
+        public Bindable(T value) => this.value = value;
+
+        private T value;
+        private Action<T> onChanged;
+
+        /// <inheritdoc/>
+        public void Bind(Action<T> action) => onChanged += action;
+        
+        /// <inheritdoc/>
+        public void BindAndInvoke(Action<T> action) {
+            onChanged += action;
+            action?.Invoke(value);
+        }
+
+        /// <inheritdoc/>
+        public void Unbind(Action<T> action) => onChanged -= action;
+
+        /// <summary>
+        /// Clears all actions subscribed to the bindable's onChanged event
+        /// </summary>
+        public void ClearBindings() => onChanged = null;
+    }
+}
---
+++ SpinModdingUtilities/Utilities/IReadOnlyBindable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SMU.Utilities {
+    /// <summary>
+    /// A read-only interface for a bindable
+    /// </summary>
+    public interface IReadOnlyBindable<T> {
+        /// <summary>
+        /// The stored value
+        /// </summary>
+        public T Value { get; }
+        
+        /// <summary>
+        /// Subscribes an action to the bindable's onChanged event
+        /// </summary>
+        /// <param name="action">The action to subscribe</param>
+        public void Bind(Action<T> action);
+
+        /// <summary>
+        /// Subscribes an action to the bindable's onChanged event and invokes the action immediately
+        /// </summary>
+        /// <param name="action">The action to subscribe and invoke</param>
+        public void BindAndInvoke(Action<T> action);
+
+        /// <summary>
+        /// Unsubscribes an action from the bindable's onChanged event
+        /// </summary>
+        /// <param name="action">The action to unsubscribe</param>
+        public void Unbind(Action<T> action);
+    }
+}
