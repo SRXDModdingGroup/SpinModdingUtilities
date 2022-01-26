929d0017016e6004a662c4d373acee5b869d16db
Dominic Aglialoro <domiaglialoro@gmail.com>
2022-01-25 16:18:26 -0800

    Version

--- SpinModdingUtilities/PluginInfo.cs
+++ SpinModdingUtilities/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "com.pink.spinrhythm.moddingutils";
         public const string PLUGIN_NAME = "Spin Modding Utilities";
-        public const string PLUGIN_VERSION = "1.0.2";
+        public const string PLUGIN_VERSION = "1.0.3";
     }
 }
