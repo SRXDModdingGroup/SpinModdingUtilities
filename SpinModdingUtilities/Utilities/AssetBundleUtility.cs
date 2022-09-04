814f3bb97c5e06e2aa079199484ea56f2cb41cde
Dominic Aglialoro <domiaglialoro@gmail.com>
2022-09-03 21:13:46 -0700

    Remove bundle folder requirement

--- SpinModdingUtilities/Utilities/AssetBundleUtility.cs
+++ SpinModdingUtilities/Utilities/AssetBundleUtility.cs
@@ -12,6 +12,27 @@ namespace SMU {
         private static Dictionary<string, AssetBundle> assetBundles = new Dictionary<string, AssetBundle>();
 
         /// <summary>
+        /// Unloads an asset bundle with the given name in the BepInEx\assetBundles folder
+        /// </summary>
+        /// <param name="name">The name of the asset bundle</param>
+        /// <remarks>Asset bundle must reside in a folder named *name*_bundle</remarks>
+        public static void UnloadAssetBundle(string name) => UnloadAssetBundle(bundlesPath, name);
+
+        /// <summary>
+        /// Unloads an asset bundle with the given name in a given folder
+        /// </summary>
+        /// <param name="directory">The folder which contains the asset bundle's folder</param>
+        /// <param name="name">The name of the asset bundle</param>
+        /// <remarks>Asset bundle must reside in a folder named *name*_bundle</remarks>
+        public static void UnloadAssetBundle(string directory, string name) {
+            if (!TryGetBundlePath(directory, name, out string path) || !assetBundles.TryGetValue(path, out var bundle))
+                return;
+            
+            bundle.Unload(true);
+            assetBundles.Remove(path);
+        }
+
+        /// <summary>
         /// Attempts to get an asset bundle with the given name in the BepInEx\assetBundles folder
         /// </summary>
         /// <param name="name">The name of the asset bundle</param>
@@ -32,18 +53,19 @@ namespace SMU {
             if (assetBundles.TryGetValue(name, out bundle))
                 return true;
 
-            string path = Path.Combine(directory, $"{name}_bundle\\{name}");
-
-            if (!File.Exists(path)) {
-                Plugin.LogWarning($"Asset bundle {path} could not be found");
-                
+            if (!TryGetBundlePath(directory, name, out string path))
                 return false;
-            }
-            
+
             bundle = AssetBundle.LoadFromFile(path);
             assetBundles.Add(name, bundle);
 
             return true;
+        }
+
+        private static bool TryGetBundlePath(string directory, string name, out string path) {
+            path = Path.Combine(directory, name);
+
+            return File.Exists(path);
         }
     }
 }
